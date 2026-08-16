25fd9ab83b1349706848f3d80a528c8c3d72b965
Sabareesh Padmanabhan <sabareeshpadmanabhan4@gmail.com>
2026-08-17 02:32:54 +0530

    feat: mount otp router under auth routes

--- src/modules/auth/routes/router.ts
+++ src/modules/auth/routes/router.ts
@@ -1,8 +1,9 @@
 import Elysia from "elysia";
 import { emailRouter } from "../providers/email";
+import { otpRouter } from "../providers/OTP";
 const routerConfig={
     prefix:'/auth'
 }
-const authRoutes = new Elysia(routerConfig).use(emailRouter)
+const authRoutes = new Elysia(routerConfig).use(emailRouter).use(otpRouter)
 
 export default authRoutes;
