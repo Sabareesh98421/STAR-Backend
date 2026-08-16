d01cfd082154b6bcfc4923a18d98ee4674ea3ba2
Sabareesh Padmanabhan <sabareeshpadmanabhan4@gmail.com>
2026-08-17 02:32:54 +0530

    feat: add redis config

---
+++ src/config/redis.config.ts
@@ -0,0 +1,9 @@
+// redis.config.ts
+import { envNumber } from './env.util';
+
+export const redisConfig = {
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    port: envNumber(process.env.REDIS_PORT, 6379),
+    password: process.env.REDIS_PASSWORD || undefined,
+    db: envNumber(process.env.REDIS_DB, 0),
+};
