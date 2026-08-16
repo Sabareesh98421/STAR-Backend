a355ddd1a90298cf6b0b1fe43e979079eae8ce92
Sabareesh Padmanabhan <sabareeshpadmanabhan4@gmail.com>
2026-08-17 02:32:54 +0530

    feat: add config module barrel

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+// index.ts
+export { appConfig } from './app.config';
+export { databaseConfig } from './database.config';
+export { redisConfig } from './redis.config';
+export { mailerConfig } from './mailer.config';
+export { otpConfig } from './otp.config';
+export { serverConfig } from './server.config';
+export { loggerConfig } from './logger.config';
